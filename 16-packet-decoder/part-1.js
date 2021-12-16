75c23893fdf48e84f916e4aa80b9aac733acde4e
Stoo Goff <sgoff@nerdwallet.com>
2021-12-16 14:28:33 +0000

    operator_0 now returns the remaining bits rather than consuming them

--- 16-packet-decoder/part-1.js
+++ 16-packet-decoder/part-1.js
@@ -47,27 +47,28 @@ const handlers = {
 	operator_0: function(input) {
 		let { value, bits } = getSubPackets(input, 15)
 		const subPackets = bits.substring(0, value)
-console.log('op0')
-		return parsePackets(subPackets)
+		const { version, _ } = parsePackets(subPackets)
+
+		return { version, bits: bits.substring(value) }
 	},
 
 	// here value is the total number of subpackets
 	operator_1: function(input) {
 		let { value, bits } = getSubPackets(input, 11)
-console.log('op1')
+
 		return parsePackets(bits, value)
 	},
 }
 
 // Test data
-const testData = {
-	//'D2FE28'                        : 6,  // first test
-	//'38006F45291200'                : 9,  // operator 0 packet
-	//'EE00D40C823060'                : 14, // operator 1 packet
-	//'8A004A801A8002F478'            : 16,
+/*const testData = {
+	'D2FE28'                        : 6,  // first test
+	'38006F45291200'                : 9,  // operator 0 packet
+	'EE00D40C823060'                : 14, // operator 1 packet
+	'8A004A801A8002F478'            : 16,
 	'620080001611562C8802118E34'    : 12, // this failed
-	//'C0015000016115A2E0802F182340'  : 23, // this failed
-	//'A0016C880162017C3686B18A3D4780': 31, 
+	'C0015000016115A2E0802F182340'  : 23, // this failed
+	'A0016C880162017C3686B18A3D4780': 31, 
 }
 
 
@@ -80,12 +81,12 @@ Object.keys(testData).forEach(key => {
 
 	console.log(`Result: ${output.version}`)
 	console.log(output.version === testData[key])
-})
+})*/
 
-/*const input = convertHexToBin(read('16-packet-decoder/data'))
+const input = convertHexToBin(read('16-packet-decoder/data'))
 const output = parsePackets(input)
 
-console.log(`Result: ${output.version}`)*/
+console.log(`Result: ${output.version}`)
 
 
 function convertHexToBin(input) {
